226e10149824083bc4ab2b1d7bf52f1fcc4b497d
GeraldCitadel <141914060+GeraldCitadel@users.noreply.github.com>
2024-11-05 14:45:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000
 // middlewares
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.use((req, res, next) => {
