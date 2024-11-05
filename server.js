55db2831cc9dc4458fa90d0417fb50a7556c5eec
GeraldCitadel <141914060+GeraldCitadel@users.noreply.github.com>
2024-11-05 14:20:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || 4000
 
 
 // middlewares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use(cors())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
