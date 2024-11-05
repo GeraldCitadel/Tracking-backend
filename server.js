43e2f94cbcb303f66cbb20371be8490948abe0e5
GeraldCitadel <141914060+GeraldCitadel@users.noreply.github.com>
2024-11-05 14:54:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
