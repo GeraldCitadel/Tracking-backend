10d285bf108e09e07733c465d4f4cf19cae581f8
Gerald Citadel <geraldcitadel@gmail.com>
2024-11-06 11:19:53 +0300

    working on vercel deployment compatibility

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import fs from "node:fs/promises"
 import jwt from 'jsonwebtoken'
+import cors from 'cors'
 
 
 // app config
@@ -11,6 +12,7 @@ const port = process.env.PORT || 4000
 
 
 // middlewares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -18,7 +20,7 @@ app.use(express.static('public'))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
